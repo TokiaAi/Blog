2c93de2798cdb7d4e26cbf2537e923b20be1f90c
toki-ai <lucngan2004@gmail.com>
2023-12-18 01:41:34 +0700

    add input

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("hello");
-})
+    res.render('index.ejs');
+});
 
 app.listen(port, () => {
     console.log(`Server already at port ${port}.`)
 }); 
 
+// The application will allow users to create and view blog posts. 
+// Posts will not persist between sessions as no database will be used in this version of the application.
 // Features
 // 1. Post Creation: Users should be able to create new posts.
 
