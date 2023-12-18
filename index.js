0e981b8745ab22ad060744aca6621a61eec72af8
toki-ai <lucngan2004@gmail.com>
2023-12-18 18:01:20 +0700

    last

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.render('index.ejs');
 });
 
 app.post("/submit", (req, res) => {
-    res.render ('read.ejs');
-})
+    const feeling = req.body['feeling'];
+    const describe = req.body['describe'];
+    res.render ('read.ejs', {feeling, describe});
+});
 
 app.listen(port, () => {
     console.log(`Server already at port ${port}.`);
