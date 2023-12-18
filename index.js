64f7cb652062e4628d827f97ff4d0ff6c29f2f00
toki-ai <lucngan2004@gmail.com>
2023-12-18 18:02:45 +0700

    final

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
