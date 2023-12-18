2062998ae5231cf801031c46726bc39ef52d51bc
toki-ai <lucngan2004@gmail.com>
2023-12-18 17:26:39 +0700

    add style

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.render('index.ejs');
 });
 
+app.post("/submit", (req, res) => {
+    res.render ('read.ejs');
+})
+
 app.listen(port, () => {
-    console.log(`Server already at port ${port}.`)
+    console.log(`Server already at port ${port}.`);
 }); 
 
 // The application will allow users to create and view blog posts. 
